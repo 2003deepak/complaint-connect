69e751f858f28e6b3cab337405435009da91b91a
= <=>
2024-08-23 15:28:25 +0530

    depolyment changes

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.use("/worker", workerRouter);
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
